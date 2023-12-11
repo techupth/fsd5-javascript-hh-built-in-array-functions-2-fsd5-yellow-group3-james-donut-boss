916f2c40a58145aa6c75b134b1b6641cff318dab
lacitadelle <jkomala99@gmail.com>
2023-12-11 20:59:42 +0700

    first commit

--- ex-8.js
+++ ex-8.js
@@ -374,5 +374,15 @@ const bills = [
 ];
 
 // Start coding here
+const totalPaidByLocation = bills.reduce((acc, bill) => {
+    //ฺ ถ้ายังไม่มี key location ให้ set key เป็นชื่อจังหวัด และ value เป็น 0
+    if (!(bill.location in acc)) {
+        acc[bill.location] = 0;
+    }
+    // พอมาถึงตรงนี้ acc[bill.location] จะไม่เป็น undefined สามารถบวกค่าเช้าไปได้
+    acc[bill.location] += bill.total;
+    // return object (acc)umulator  
+    return acc;
+}, {});
 
-const totalPaidByLocation;
+
